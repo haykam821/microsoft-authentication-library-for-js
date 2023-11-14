17c5b3233c00b2d2ec7445d2792bcd62a3b291a4
github-action-benchmark <github@users.noreply.github.com>
2023-11-14 02:00:24 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for aedda1e187df946e3f2be8f53e25d8b95b28e751

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699333781553,
+  "lastUpdate": 1699927224569,
   "repoUrl": "https://github.com/haykam821/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -76,6 +76,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.76%",
             "unit": "ops/sec",
             "extra": "213 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "travis.walker@microsoft.com",
+            "name": "Travis Walker",
+            "username": "trwalke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aedda1e187df946e3f2be8f53e25d8b95b28e751",
+          "message": "VS Code debugger setup for contributing. (#6671)\n\nAdding section for VS Code debugger setup\r\n\r\n---------\r\n\r\nCo-authored-by: trwalke <trwalke@microsoft.com>",
+          "timestamp": "2023-11-13T22:37:37Z",
+          "tree_id": "11dcf64b3ed7b5ddd759f838deee75f3772a7162",
+          "url": "https://github.com/haykam821/microsoft-authentication-library-for-js/commit/aedda1e187df946e3f2be8f53e25d8b95b28e751"
+        },
+        "date": 1699927223316,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 184801,
+            "range": "±2.06%",
+            "unit": "ops/sec",
+            "extra": "221 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 204961,
+            "range": "±2.09%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
           }
         ]
       }
