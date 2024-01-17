52b8e9b6c3ee19aa6b22636390b97d02521ac89e
github-action-benchmark <github@users.noreply.github.com>
2024-01-17 02:37:01 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 603098e62124b90c13dcd6e57a7d83d95cc07ce8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702769255546,
+  "lastUpdate": 1705459021511,
   "repoUrl": "https://github.com/haykam821/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -190,6 +190,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.98%",
             "unit": "ops/sec",
             "extra": "219 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hemoral@microsoft.com",
+            "name": "Hector Morales",
+            "username": "hectormmg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "603098e62124b90c13dcd6e57a7d83d95cc07ce8",
+          "message": "Fix default sample static file paths (#6786)",
+          "timestamp": "2024-01-09T15:37:23-08:00",
+          "tree_id": "cb1d11ab2471cb02ae4369e3a3cc53dca4d94e11",
+          "url": "https://github.com/haykam821/microsoft-authentication-library-for-js/commit/603098e62124b90c13dcd6e57a7d83d95cc07ce8"
+        },
+        "date": 1705459019820,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 184648,
+            "range": "±2.13%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 182486,
+            "range": "±1.91%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
           }
         ]
       }
